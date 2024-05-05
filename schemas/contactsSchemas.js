d85e87824645a400a345c73da88d724ff5c78c36
culplate <culplate@gmail.com>
2024-05-05 20:32:58 +0200

    Added/modified schemas

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,11 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-  name: Joi.string()
-    .required()
-    .messages({ "any.required": "Name is required" }),
+  name: Joi.string().required().min(3).max(20).messages({
+    "any.required": "Name is required",
+    "string.min": "Min. name length is 3 symbols",
+    "string.max": "Max. name length is 20 symbols",
+  }),
   email: Joi.string().email({ minDomainSegments: 2 }).required().messages({
     "any.required": "Email is required.",
     "string.email": "Invalid email.",
@@ -20,4 +22,23 @@ export const createContactSchema = Joi.object({
     .required(),
 });
 
-export const updateContactSchema = Joi.object({});
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
+    "any.required": "Name is required",
+    "string.min": "Min. name length is 3 symbols",
+    "string.max": "Max. name length is 20 symbols",
+  }),
+  email: Joi.string().email({ minDomainSegments: 2 }).messages({
+    "any.required": "Email is required.",
+    "string.email": "Invalid email.",
+  }),
+  phone: Joi.string()
+    .regex(/^[0-9]+$/)
+    .min(5)
+    .max(15)
+    .messages({
+      "string.pattern.base": `Phone number must include digits only.`,
+      "string.min": "Minimal number length is 5.",
+      "string.max": "Maximal number length is 15",
+    }),
+});
