ef0e21594fe00e0522f09c6dce9b4ad5fe6712f7
culplate <culplate@gmail.com>
2024-05-22 12:24:54 +0200

    Added auth routes

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import contactsRouter from "./routes/contactsRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 import "./db/db.js";
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
---
+++ routes/authRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const authRouter = express.Router();
+
+authRouter.post("/register", registerUser);
+authRouter.post("/login", loginUser);
+authRouter.post("/logout", logoutUser);
+
+export default authRouter;
