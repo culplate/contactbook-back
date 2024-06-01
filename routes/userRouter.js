f44394d03c8c8530fe6eb7f84bd17f0cc861c892
culplate <culplate@gmail.com>
2024-06-01 02:21:32 +0200

    added routes for email verification

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { getAvatar, updateAvatar } from "../controllers/userControllers.js";
+import {
+  getAvatar,
+  updateAvatar,
+  verifyUser,
+  reVerifyUser,
+} from "../controllers/userControllers.js";
 import { authMiddleware } from "../middlewares/auth.js";
 import { avatarUploadMiddleware } from "../middlewares/avatarUpload.js";
 import user from "../models/user.js";
 
 const userRouter = express.Router();
 
+userRouter.get("/verify/:verificationToken", verifyUser);
+userRouter.post("/verify", reVerifyUser);
 userRouter.get("/avatars", authMiddleware, getAvatar);
 userRouter.patch(
   "/avatars",
