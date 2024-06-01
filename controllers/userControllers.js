3805460f6a59f5eb86570d65cc2d30cfc997d89b
culplate <culplate@gmail.com>
2024-06-01 02:25:31 +0200

    Controllers to verify email + resend verification mail

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,6 +3,65 @@ import User from "../models/user.js";
 import * as fs from "node:fs/promises";
 import path from "node:path";
 import Jimp from "jimp";
+import { sendMail } from "../helpers/mail.js";
+import { emailSchema } from "../schemas/usersSchemas.js";
+
+export const verifyUser = async (req, res, next) => {
+  const { verificationToken } = req.params;
+
+  try {
+    const user = await User.findOne({ verificationToken: verificationToken });
+
+    if (user === null) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: null,
+    });
+
+    return res.status(200).send({ message: "Verification successful" });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const reVerifyUser = async (req, res, next) => {
+  const { email } = req.body;
+  const { error, value } = emailSchema.validate({ email });
+
+  if (error) {
+    return res.status(400).send({ message: error.message });
+  }
+
+  try {
+    const user = await User.findOne({ email: value.email });
+
+    if (user === null) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    if (user.verify === true) {
+      return res
+        .status(400)
+        .send({ message: "Verification has already been passed" });
+    }
+
+    //TODO put message template to another file
+    sendMail({
+      to: user.email,
+      from: "culplate@gmail.com",
+      subject: "Welcome to ContactBook!",
+      html: `To finish registration, please, click the <a href='http://localhost:${process.env.PORT}/api/users/verify/${user.verificationToken}'>link</a>`,
+      text: `To finish registration, please, click the <a href='http://localhost:${process.env.PORT}/api/users/verify/${user.verificationToken}'>link</a>`,
+    });
+
+    return res.status(200).send({ message: "Verification email sent" });
+  } catch (e) {
+    next(e);
+  }
+};
 
 export const getAvatar = async (req, res, next) => {
   try {
