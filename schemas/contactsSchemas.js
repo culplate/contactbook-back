0cfae16eb905cae359cb9abc7e5164442331a3f8
culplate <culplate@gmail.com>
2024-11-09 00:42:36 +0100

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,19 +1,31 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().required().min(3).max(20).messages({
+  name: Joi.string().required().trim().min(3).max(30).messages({
     "any.required": "Name is required",
     "string.min": "Min. name length is 3 symbols",
-    "string.max": "Max. name length is 20 symbols",
+    "string.max": "Max. name length is 30 symbols",
   }),
-  email: Joi.string().email({ minDomainSegments: 2 }).messages({
-    "any.required": "Email is required.",
-    "string.email": "Invalid email.",
-  }),
+  email: Joi.string()
+    .email({ minDomainSegments: 2 })
+    .trim()
+    .allow("")
+    .empty("")
+    .default(null)
+    .min(5)
+    .max(50)
+    .messages({
+      "any.required": "Email is required.",
+      "string.email": "Invalid email.",
+    }),
   number: Joi.string()
     .regex(/^\+?[0-9]+$/)
     .min(5)
-    .max(15)
+    .max(20)
+    .trim()
+    .allow("")
+    .empty("")
+    .default(null)
     .messages({
       "string.pattern.base": `Phone number must start with an optional '+' and include digits only.`,
       "string.min": "Minimal number length is 5.",
@@ -22,19 +34,19 @@ export const createContactSchema = Joi.object({
 }).min(1);
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).messages({
+  name: Joi.string().min(3).max(30).messages({
     "any.required": "Name is required",
     "string.min": "Min. name length is 3 symbols",
-    "string.max": "Max. name length is 20 symbols",
+    "string.max": "Max. name length is 30 symbols",
   }),
-  email: Joi.string().email({ minDomainSegments: 2 }).messages({
+  email: Joi.string().email({ minDomainSegments: 2 }).min(5).max(50).messages({
     "any.required": "Email is required.",
     "string.email": "Invalid email.",
   }),
-  phone: Joi.string()
-    .regex(/^[0-9]+$/)
+  number: Joi.string()
+    .regex(/^\+?[0-9]+$/)
     .min(5)
-    .max(15)
+    .max(20)
     .messages({
       "string.pattern.base": `Phone number must include digits only.`,
       "string.min": "Minimal number length is 5.",
